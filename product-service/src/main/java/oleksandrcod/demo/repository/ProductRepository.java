e35e4dd8e0c691bfbc24ebd18282d1b8f81dffa5
OleksandrCod <karpo99old@gmail.com>
2023-12-24 14:30:50 +0200

    add observability with prometheus, loki, tempo. Explore with grafana

--- product-service/src/main/java/oleksandrcod/demo/repository/ProductRepository.java
+++ product-service/src/main/java/oleksandrcod/demo/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package oleksandrcod.demo.repository;
 
+import io.micrometer.observation.annotation.Observed;
 import oleksandrcod.demo.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+@Observed
 public interface ProductRepository extends MongoRepository<Product, Long> {
 }
